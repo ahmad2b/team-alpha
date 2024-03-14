4f6148102174a1eaa2136c20817ad8d847b20ffe
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-14 15:39:03 +0500

    Refactor root route and add new route for retrieving all todos

--- fastapi/todo/src/main.py
+++ fastapi/todo/src/main.py
@@ -6,9 +6,9 @@ from src.todofn import create_todo, get_todo_by_id, Todo, get_all_todos
 app = FastAPI()
 
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/todo/{item_id}")
@@ -24,6 +24,6 @@ def create_todo_api(title: str, description: str):
     return todo
 
 
-@app.get("/")
+@app.get("/todo")
 def get_todos():
     return get_all_todos()
